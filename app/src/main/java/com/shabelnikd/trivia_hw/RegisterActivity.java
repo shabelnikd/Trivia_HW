2385e37b8ef4f4aa8f4be6db960bd09f90b329e3
shabelnikd <shabelnikdanil.main@gmail.com>
2024-12-06 06:10:20 +0600

    Added new Activity and added change two activity with onBackPressedDispatcher

--- app/src/main/java/com/shabelnikd/trivia_hw/RegisterActivity.java
+++ app/src/main/java/com/shabelnikd/trivia_hw/RegisterActivity.java
@@ -11,26 +11,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
 
-        Button upWithEmail = findViewById(R.id.email_button);
+        ImageButton forwardButton = findViewById(R.id.register_forward_button);
 
-        upWithEmail.setOnClickListener(click -> {
-            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-            startActivity(intent);
+        forwardButton.setOnClickListener(click -> {
+            getOnBackPressedDispatcher().onBackPressed();
+            finish();
         });
 
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.register), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
 }
